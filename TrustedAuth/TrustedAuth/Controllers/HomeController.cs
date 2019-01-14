cfbd2bc92520ff408e7558096ffd0de211607a5c
DESKTOP-PG56K6G\ipydev <ironpython2001@gmail.com>
2019-01-14 08:48:37 +0530

    initial

---
+++ TrustedAuth/TrustedAuth/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrustedAuth.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            
+            var request = (HttpWebRequest)WebRequest.Create("https://104.42.181.6" + "/trusted");
+            var encoding = new UTF8Encoding();
+            var postData = "username=tableau"  ;  //userName = "TabGenericUser"
+
+            byte[] data = encoding.GetBytes(postData);
+
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = data.Length;
+
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+            var response = (HttpWebResponse)request.GetResponse();
+            var s= new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
